2f692e1a6b99c2d1834b04352330b51750bf6eee
Aleksandra Stojanovic <alexandrastojanovic2@gmail.com>
2020-10-17 11:26:38 +0200

    SacuvajRezervacijuServlet

--- src/java/obrada/SacuvajRezervacijuServlet.java
+++ src/java/obrada/SacuvajRezervacijuServlet.java
@@ -7,6 +7,7 @@ package obrada;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -34,24 +35,53 @@ public class SacuvajRezervacijuServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         HttpSession sesija = request.getSession();
-        int korisnik_id = (Integer) sesija.getAttribute("korisnik_id");
-        if (korisnik_id == -1) {
+        int korisnikId = (Integer) sesija.getAttribute("korisnik_id");
+        if (korisnikId == -1) {
             response.sendRedirect("proveraPrijavljen");
             return;
         }
 
         String tip = sesija.getAttribute("tip").toString();
         String putanja;
+
+        DogadjajBaza dogadjajBaza = new DogadjajBaza();
+        Dogadjaj dogadjaj = dogadjajBaza.find(Integer.parseInt(request.getParameter("dogadjaj_id")));
+        RezervacijaBaza rezervacijaBaza = new RezervacijaBaza();
+        ArrayList<Rezervacija> rezervacije = rezervacijaBaza.all();
+
+        Rezervacija rezervacija = napraviRezervaciju(sesija, request);
+        StrukturaUlaznicaBaza strukturaUlaznicaBaza = new StrukturaUlaznicaBaza();
+        StrukturaUlaznica struktura = strukturaUlaznicaBaza.find(rezervacija.getStruktura_id());
+        
+        int brojUlaznica = Integer.parseInt(request.getParameter("broj_ulaznica"));
         switch (tip) {
             case Korisnik.TIP_REGISTROVANI_KORISNIK:
-                DogadjajBaza dogadjajBaza = new DogadjajBaza();
-                Dogadjaj dogadjaj = dogadjajBaza.find(Integer.parseInt(request.getParameter("dogadjaj_id")));
-                if(LocalDateTime.now().isAfter(dogadjaj.getDatum_i_vreme().minusDays(2))){
+                if (LocalDateTime.now().isAfter(dogadjaj.getDatum_i_vreme().minusDays(2))) {
                     //poruka ulaznice je moguce kupiti samo na blagajni
                     response.sendRedirect("index");
                     return;
                 }
                 putanja = "mojeUlaznice";
+                //prodjem kroz sve rezervacije za tog korisnika i nadjem po strukturi id, i onda sabiram broj_ulaznica
+                //i poredim sa granicomPoKorisniku
+                //i rezervacija vazi dva dana, nakon cega registrovani korisnik ne moze da rezervise za taj dogadjaj, ako nije placeno
+                
+                for (Rezervacija rezervacijaKorisnika : rezervacije) {
+                    int ukupanBrojUlaznica = brojUlaznica;
+                    if (rezervacijaKorisnika.getKorisnik_id() == korisnikId) {
+                        if (rezervacijaKorisnika.getDogadjaj_id() == dogadjaj.getId()
+                                && !Rezervacija.STATUS_PLACENO.equals(rezervacijaKorisnika.getStatus())) {
+                            putanja = "dogadjajPojedinacno"; //greska ne moze da rezervise za taj dogadjaj dok ne plati
+                            request.setAttribute("dogadjajId", dogadjaj.getId());
+                        }
+                        if (rezervacijaKorisnika.getStruktura_id() == struktura.getId()) {
+                            ukupanBrojUlaznica += rezervacijaKorisnika.getBroj_ulaznica();
+                            if (struktura.getGranicaPoKorisniku() - ukupanBrojUlaznica < 0) {
+                                response.sendRedirect("index"); //greska ne moze da rezervise vise od granicaPoKorisniku karata
+                            }
+                        }
+                    }
+                }
                 break;
             case Korisnik.TIP_BLAGAJNIK:
                 putanja = "potvrdaRezervacije";
@@ -61,45 +91,28 @@ public class SacuvajRezervacijuServlet extends HttpServlet {
                 return;
         }
 
-        RezervacijaBaza rezervacijaBaza = new RezervacijaBaza();
-        Rezervacija rezervacija = new Rezervacija();
-        /*if (request.getParameter("struktura_id") != null) {
-            rezervacija = rezervacijaBaza.find(Integer.parseInt(request.getParameter("struktura_id")));
-        }*/
-        //ovde for petlja da prolazi kroz checkboxove? ili je to preko js?             
-
-        rezervacija.setDogadjaj_id(Integer.parseInt(request.getParameter("dogadjaj_id")));
-        rezervacija.setKorisnik_id(korisnik_id);
-        rezervacija.setStruktura_id(Integer.parseInt(request.getParameter("struktura_id")));
-        StrukturaUlaznicaBaza strukturaUlaznicaBaza = new StrukturaUlaznicaBaza();
-        StrukturaUlaznica struktura = strukturaUlaznicaBaza.find(rezervacija.getStruktura_id());
-        //prodjem kroz sve rezervacije za tog korisnika i nadjem po strukturi id, i onda sabiram broj_ulaznica
-        //i poredim sa granicomPoKorisniku
-        int brojUlaznica = Integer.parseInt(request.getParameter("broj_ulaznica"));
-        if (struktura.getPreostalo_ulaznica() - brojUlaznica >= 0
-                && struktura.getGranicaPoKorisniku() - brojUlaznica >= 0) {
-
+        if (struktura.getPreostalo_ulaznica() - brojUlaznica >= 0) {
             rezervacija.setBroj_ulaznica(brojUlaznica);
             struktura.setPreostalo_ulaznica(struktura.getPreostalo_ulaznica() - rezervacija.getBroj_ulaznica());
             strukturaUlaznicaBaza.save(struktura);
             rezervacija = rezervacijaBaza.save(rezervacija);
-            RequestDispatcher rd = request.getRequestDispatcher(putanja);
+
             if (rezervacija.getId() > 0) {
                 request.setAttribute("rezervacija_id", rezervacija.getId());
-                
             } else {
                 //poruka da nije uspesno
-                response.sendRedirect("dogadjajPojedinacno");
-                return;
+//                response.sendRedirect("dogadjajPojedinacno");
+                putanja = "dogadjajPojedinacno";
+                request.setAttribute("dogadjajId", dogadjaj.getId());
             }
-            rd.forward(request, response);
-
         } else {
             //nema vise dostupnih ulaznica, molimo izaberite drugu kategoriju.
             //ili jos bolje, da se prikazuje odmah poruka cim ide na submit, ali
             //mislim da svakako ostaje i ovde provera
+            response.sendRedirect("index");
         }
-
+        RequestDispatcher rd = request.getRequestDispatcher(putanja);
+        rd.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -141,4 +154,12 @@ public class SacuvajRezervacijuServlet extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
+    private Rezervacija napraviRezervaciju(HttpSession session, HttpServletRequest request) {
+        int korisnikId = (int) session.getAttribute("korisnik_id");
+        Rezervacija rezervacija = new Rezervacija();
+        rezervacija.setDogadjaj_id(Integer.parseInt(request.getParameter("dogadjaj_id")));
+        rezervacija.setKorisnik_id(korisnikId);
+        rezervacija.setStruktura_id(Integer.parseInt(request.getParameter("struktura_id")));
+        return rezervacija;
+    }
 }
